3b79f821596bf80e78e7cddfa7a2c09e2fd064c5
SoloX <57513371+Monkey-X-Byte@users.noreply.github.com>
2024-07-09 11:48:42 +0800

    Mod：修改
    修改

--- js/script.js
+++ js/script.js
@@ -1,76 +1,25 @@
-/*
-Theme: Flatfy Theme
-Author: Andrea Galanti
-Bootstrap Version 
-Build: 1.0
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-*/
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-$(window).load(function() { 
-	//Preloader 
-	$('#status').delay(300).fadeOut(); 
-	$('#preloader').delay(300).fadeOut('slow');
-	$('body').delay(550).css({'overflow':'visible'});
-})
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
-$(document).ready(function() {
-		//animated logo
-		$(".navbar-brand").hover(function () {
-			$(this).toggleClass("animated shake");
-		});
-		
-		//animated scroll_arrow
-		$(".img_scroll").hover(function () {
-			$(this).toggleClass("animated infinite bounce");
-		});
-		
-		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
-		wow = new WOW(
-		{
-			mobile: false
-		});
-		wow.init();
-		
-		//MagnificPopup
-		$('.image-link').magnificPopup({type:'image'});
-
-
-		// OwlCarousel N1
-		$("#owl-demo").owlCarousel({
-			autoPlay: 3000,
-			items : 3,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3]
-		});
-
-		// OwlCarousel N2
-		$("#owl-demo-1").owlCarousel({
-			  navigation : false, // Show next and prev buttons
-			  slideSpeed : 300,
-			  paginationSpeed : 400,
-			  singleItem:true
-		});
-
-		//SmothScroll
-		$('a[href*=#]').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-			&& location.hostname == this.hostname) {
-					var $target = $(this.hash);
-					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-					if ($target.length) {
-							var targetOffset = $target.offset().top;
-							$('html,body').animate({scrollTop: targetOffset}, 600);
-							return false;
-					}
-			}
-		});
-		
-		//Subscribe
-		new UIMorphingButton( document.querySelector( '.morph-button' ) );
-		// for demo purposes only
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
-			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
-		} );
-
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  reset: true,
 });
 
+sr.reveal(".home-text", { delay: 200, origin: "top" });
+sr.reveal(".home-img", { delay: 400, origin: "top" });
+sr.reveal(".about, .cta, .resume, .contact, .footer", {
+  delay: 200,
+  origin: "top",
+});
